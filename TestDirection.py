9bdf3905820e0b4ae0b9b3ba6b18929f02573cba
BartmanDilaw <fraternite76-alain@yahoo.fr>
2022-10-23 23:34:02 +0200

    Ajout d'un test sur direction de la tortue

---
+++ TestDirection.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+my_turtle = Turtle()   # create a turtle
+my_turtle.color('green')      # set the color
+my_turtle.forward(50)         # draw a green line of length 50
+my_turtle.up()                # lift up the tail
+my_turtle.forward(50)         # move forward 50 without drawing
+my_turtle.right(45)           # change direction to the right, left works too
+my_turtle.down()              # put the tail down
+my_turtle.backward(100)       # draw a green line 100 units long
+my_turtle.right(45)           # change direction to the right, left works too
+my_turtle.backward(100)       # draw a green line 100 units long
+
+exitonclick()
